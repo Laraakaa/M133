ed0237b77527f91ee6a56458956ea10e36cd0d55
Lars Bärtschi <lars.baertschi@bluewin.ch>
2017-08-17 09:22:17 +0200

    Initial commit

---
+++ Lektion_01/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="de">
+  <head>
+    <meta charset="utf-8">
+    <title>Lektion 1</title>
+  </head>
+  <body>
+    <h1>Hello World mit JavaScript</h1>
+    <script>
+      alert('Hello World');
+    </script>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+  <html>
+    <title>Projekt M133</title>
+  </html>
+  <body>
+    <h1>
+      Projekte M133
+    </h1>
+    <ol>
+      <li>
+        <a href="Lektion_01">Lektion 1</a>
+      </li>
+    </ol>
+  </body>
+</html>

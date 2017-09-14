f44b76356cfa1d81a0e4f12f454fc2616ef6a4a6
Lars Bärtschi <lars.baertschi@bluewin.ch>
2017-09-14 09:29:32 +0200

    WIP: State, Game mechanics

--- TicTacToe/index.php
+++ TicTacToe/index.php
@@ -12,38 +12,40 @@
     <div class="gameGrid">
       <div class="gameGridRow">
         <div class="gameGridColumn">
-          <button class="gameGridButton">
+          <button class="gameGridButton" id="game1" disabled onClick="gameLoop(1)">
         </div>
         <div class="gameGridColumn">
-          <button class="gameGridButton">
+          <button class="gameGridButton" id="game2" disabled onClick="gameLoop(2)">
         </div>
         <div class="gameGridColumn">
-          <button class="gameGridButton">
-        </div>
-      </div>
-      <div class="gameGridRow">
-        <div class="gameGridColumn">
-          <button class="gameGridButton">
-        </div>
-        <div class="gameGridColumn">
-          <button class="gameGridButton">
-        </div>
-        <div class="gameGridColumn">
-          <button class="gameGridButton">
+          <button class="gameGridButton" id="game3" disabled onClick="gameLoop(3)">
         </div>
       </div>
       <div class="gameGridRow">
         <div class="gameGridColumn">
-          <button class="gameGridButton">
+          <button class="gameGridButton" id="game4" disabled onClick="gameLoop(4)">
         </div>
         <div class="gameGridColumn">
-          <button class="gameGridButton">
+          <button class="gameGridButton" id="game5" disabled onClick="gameLoop(5)">
         </div>
         <div class="gameGridColumn">
-          <button class="gameGridButton">
+          <button class="gameGridButton" id="game6" disabled onClick="gameLoop(6)">
+        </div>
+      </div>
+      <div class="gameGridRow">
+        <div class="gameGridColumn">
+          <button class="gameGridButton" id="game7" disabled onClick="gameLoop(7)">
+        </div>
+        <div class="gameGridColumn">
+          <button class="gameGridButton" id="game8" disabled onClick="gameLoop(8)">
+        </div>
+        <div class="gameGridColumn">
+          <button class="gameGridButton" id="game9" disabled onClick="gameLoop(9)">
         </div>
       </div>
     </div>
+
+    <button onClick="revert()" disabled>Zurück</button>
 
     <div class="gameOptions">
       <h2>Typ des Spieles:</h2>
@@ -52,10 +54,10 @@
           <input type="radio" id="radioMenschVsMensch" onClick="selectOption('MvsM')" class="optionsRadio"><label for="radioMenschVsMensch">Mensch vs Mensch</label>
           <ul class="gameOptionsPlayerList">
             <li>
-              <label for="inputPlayerOneMvsM">Spieler 1 (Email):</label><input type="email" id="inputPlayerOneMvsM" class="optionsInput" disabled>
+              <label for="inputPlayerOneMvsM">Spieler 1 (Email):</label><input type="email" value="p1@test.ch" id="inputPlayerOneMvsM" class="optionsInput" disabled>
             </li>
             <li>
-              <label for="inputPlayerTwoMvsM">Spieler 2 (Email):</label><input type="email" id="inputPlayerTwoMvsM" class="optionsInput" disabled>
+              <label for="inputPlayerTwoMvsM">Spieler 2 (Email):</label><input type="email" value="p2@test.ch" id="inputPlayerTwoMvsM" class="optionsInput" disabled>
             </li>
           </ul>
         </li>
--- TicTacToe/script.js
+++ TicTacToe/script.js
@@ -1,7 +1,13 @@
 // Global variables
 // State is holding all the previous states
-var state = {};
+var state = [{
+  player: 1,
+  field: [0, 0, 0, 0, 0, 0, 0, 0, 0]
+}];
 var mode = "";
+
+// Configuration
+const emailPattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 // Initialize the program. Main function.
 const init = () => {
@@ -16,6 +22,58 @@ const start = () => {
     console.log('Validation failed.')
     return;
   }
+
+  // Gameloop betreten
+  gameLoop();
+}
+
+const gameLoop = (clickedElement) => {
+  // Select last state
+
+  // Nur state ändern, wenn etwas geklickt wurde.
+  if (clickedElement) {
+    // Um die mutierung zu stoppen klonen
+    const currentState = state[state.length - 1];
+    // Hiermit wird das Objekt tief geklont.
+    const nextState = JSON.parse(JSON.stringify(currentState));
+    // Spieler ändern
+    nextState.player = (currentState.player === 1) ? 2 : 1;
+    nextState.field[clickedElement - 1] = nextState.player;
+    state.push(nextState);
+    console.log(state);
+  }
+
+  const currentState = state[state.length - 1];
+  //console.log("State", state)
+  applyToField(currentState);
+}
+
+const revert = () => {
+  state = state.slice(0, -1);
+  gameLoop();
+}
+
+const applyToField = (currentState) => {
+  currentState.field.forEach((number, index) => {
+    const targetField = $("#game" + (index + 1));
+    switch(number) {
+      case 0:
+        targetField.text("");
+        targetField.css("background-color", "grey");
+        targetField.prop("disabled", false);
+        break;
+      case 1:
+        targetField.text("X");
+        targetField.prop("disabled", true);
+        targetField.css("background-color", "orange");
+        break;
+      case 2:
+        targetField.text("O");
+        targetField.prop("disabled", true);
+        targetField.css("background-color", "blue");
+        break;
+    }
+  });
 }
 
 const setStatus = (str, color) => {
@@ -52,5 +110,32 @@ const validate = () => {
     setStatus('Bitte erst eine Option auswählen.', 'red');
     return false;
   }
+  // Um beide Textfelder zu prüfen wurde hier | anstelle von || verwendet.
+  if (!validateEmail("#inputPlayerOneMvsM") | !validateEmail("#inputPlayerTwoMvsM")) {
+    setStatus('Bitte valide Email-Adressen eingeben', 'red');
+    return false;
+  }
+
+  setStatus("Eingaben korrekt!", "green");
   return true;
+}
+
+const validateEmail = (inputId) => {
+  const email = $(inputId).val();
+
+  if (emailPattern.test(email)) {
+    $(inputId).removeClass("emailInvalid");
+    return true;
+  } else {
+    // Invalid indicator
+    $(inputId).addClass("emailInvalid");
+    return false;
+  }
+}
+
+const checkForWinner = (currentState) => {
+  const fields = currentState.field;
+
+  // Senkrecht 1
+  if (fields[0])
 }
